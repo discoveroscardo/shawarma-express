e5226640122482c236a5731b1b71828664887642
OscarGarcia <discoveroscardo@gmail.com>
2025-05-02 13:41:00 +0200

    arreglando estado de bot en render

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const { Telegraf, Scenes, session } = require('telegraf');
 const mongoose = require('mongoose');
 
